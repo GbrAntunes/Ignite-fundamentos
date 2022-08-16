5f4a55844be19bb030a0c1ff77080028c6bda781
Gabriel <gbr.antunes@gmail.com>
2022-08-16 13:43:54 -0400

    initial commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+  
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1)
+
+    /**
+     * Para evitar que o React acesse um estado antigo do likeCount
+     * podemos utilizar o parâmetro (no caso) state para acessar o valor
+     * mais recente e passa-lo corretamente para o likeCount
+     */
+
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/gbrAntunes.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Antunes</strong>
+              <time title="11 de maio às 08h13" dateTime="2022-05-11">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
